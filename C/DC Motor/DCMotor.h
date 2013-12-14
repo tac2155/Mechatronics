680083b4b7b88030eabd263df639ba74b80b7707
Tim Cousins <tcousins2010@gmail.com>
2013-12-14 15:42:34 -0500

    starting CS6

---
+++ C/DC Motor/DCMotor.h
@@ -0,0 +1,19 @@
+#include <pic.h>
+	__CONFIG(11111110110010);
+
+/***********************************************************
+
+Header file for DC Motor Case Study
+
+Tim Cousins
+Chi Yung Fung
+Jamie Collier
+
+Contrains configuration and pic.h. Define the various pins with 
+names that make code easier to read. Contains function declartions
+for code to compile easier.
+
+***********************************************************/
+
+typedef	unsigned char	uint8_t;
+typedef	unsigned int	uint16_t;
