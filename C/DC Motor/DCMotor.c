2381afb2b103a01749cb71887f90db663fb85e3f
Tim Cousins <tcousins2010@gmail.com>
2013-12-18 23:06:40 -0500

    commented up

--- C/DC Motor/DCMotor.c
+++ C/DC Motor/DCMotor.c
@@ -8,64 +8,98 @@ Tim Cousins
 Jamie Collier
 Chi Yung Fung
 
+This case study controls a DC Motor with a trapezoidal voltage
+profile with 4 different modes of feedback. After the ports and
+registers are initialized, an intial voltage to overcome the 
+Coulumb friction of the motor is determined and used as the 
+reference voltage for the profile. The gains and bias are set,
+and the program waits for the user to press the button. Once the
+button is pressed, the mode is read and the motor is controlled 
+with the feedback specified by the mode. The A/D converter is 
+started to read the speed of the motor from the tachometer, and 
+an interrupt is used for whenever the conversion is finished.
+Whenver 64 readings have been made, the average is taken and used 
+as the speed of the motor to determine the required input voltage.
+
+For the profile, the reference voltage is incremented every full
+rotation until it reaches 190. Once at 190, it holds for 600
+revolutions before decrementing every revolution to 140. The revoltions
+are determined by an encoder attached to the motor. Every 100 passes
+of the encoder is equal to one revolution. During the encoder checks, 
+the program checks to see if 64 tachometer readings have been made, 
+and if they have, it determines the average speed and the required duty
+cycle.
+
+Mode 0 - Proportional Control
+Duty Cycle = Kp * error
+
+Mode 1 - Proportional + Bias Control
+Duty Cycle = Kp * error + Bias
+
+Mode 2 - Proportional - Derivative Control
+Duty Cycle = Kp * error + Kd * (error - (Previous Error))
+
+Mode 3 - Proportional - Integral - Derivative Control
+Duty Cycle = Kp * error + Kd * (error - (Previous Error)) + Ki * (total error)
+
+Outside References:
+C Programming Language, 2nd Edition
+stackoverflow.com
 ***********************************************************/
 
 void main(void)
 {
-	init();
-	
+	init();									// Run initialization routine
+	kp  	= 15;							// set proportional gain
+	kd 		= 5;							// set derivative gain
+	ki		= 0.005;						// set integral gain
+
 	while(!blackButton) {} 					// wait for button press
 	while(blackButton)  {}					// wait for release
 	delay(200);	
-
-	kp  	= 15;
-	kd 		= 5;
-	ki		= 0.005;
 		
-	//motorControl(kp, 0, 0, 0);							// delay for switch to debounce, 1200us
-
-	if(MSBHex)
+	if(MSBHex)								// if upper bit of switch hi
 	{
-		if(LSBHex)
+		if(LSBHex)							// and lower bit hi
 		{
-			mode = 3;
+			mode = 3;						// then mode 3
 		}
 		else
 		{
-			mode = 2;
+			mode = 2;						// else mode 2
 		}
 	}
-	else if(LSBHex)
+	else if(LSBHex)							// if lower bit hi and upper bit lo
 	{
-		mode = 1;
+		mode = 1;							// then mode 1 
 	}
 	else
-	{
-		mode = 0;
+	{	
+		mode = 0;							// else mode 0
 	}
 	
-	switch(mode)
+	switch(mode)							// switch case for mode
 	{
 		case 0:
-			motorControl(kp, 0, 0, 0);
+			motorControl(kp, 0, 0, 0);		// mode 0 - proportional control
 			break;
 
 		case 1:
-			motorControl(kp, 0, 0, bias);
+			motorControl(kp, 0, 0, bias);	// mode 1 - proportional control with bias
 			break;
 
-		case 2:
+		case 2:								// mode 2 - proportional derivative control
 			motorControl(kp, kd, 0, 0);
 			break;
 
-		case 3:
-			motorControl(kp, kd, ki, 0);
+		case 3:								// mode 3 - PID control
+			motorControl(kp, kd, ki, 0);	
 			break;
 	}
 
-	while(1 != 42)
+	while(1 != 42)							// Inifinite loop
 	{
-		PORTB = 0xff;						//	Flash mode error every second
+		PORTB = 0xff;						//	Flash Port B every second
 		longTimer();
 		PORTB = 0x00;
 		longTimer();
@@ -76,7 +110,7 @@ void main(void)
 
 //	Initialization
 
-void	init(void)
+void	init(void) 							// Initialize Ports and Registers
 {
 	PORTA	= 0x00;							// Clear Ports
 	PORTB 	= 0x00;
@@ -86,21 +120,18 @@ void	init(void)
 	TRISC	= 0b11111000;					// set Port C inputs and outputs
 	TRISB	= 0x00;							// Set Port B as all outputs
 
-	initISR();
+	initISR();								// ISR initialization
 	initAtoD();								// AtoD Initialization
 	initPWM();								// PWM initialization
 
-	refVoltage();
-
-	readings  = 0;
-	speed	  = 0;
+	refVoltage();							// determine reference voltage
 }
 
 void	initISR(void)						// initialize interrupts				
 {
 	INTCON	= 0b11000000;					// enable global and peripheral interrupts
-	ADIE	= 1;
-	ADIF	= 0;
+	ADIE	= 1;							// enable A/D interrupt bit
+	ADIF	= 0;							// set A/D flag to zero
 }
 
 void	initAtoD(void)						// initialize A/D
@@ -117,143 +148,158 @@ void	initPWM(void)
 	T2CON 	= 0b00000100;					// set up Timer 2
 }
 
-void	refVoltage(void)
+void	refVoltage(void)					// determine reference voltage
 {
-	velRef	= 160;
-	bias 	= velRef;
+	velRef	= 160;							// set refefence voltage at 160
+	bias 	= velRef;						// set bias equal to this value
 
 	dir 	= 1;							// Motor cw
-	CCPR1L 	= velRef;						// set reference voltage at 160
+	CCPR1L 	= velRef;						// set PWM duty cycle at 160
 	PWM 	= 1;							// start PWM
+
+	//	Wait for four passes of the eddy sensor
 
 	for (eddyCount = 0; eddyCount < 4; eddyCount++)
 	{
-		while(!eddy) {}
-		while(eddy)  {}
+		while(!eddy) {}						// while sensor not activated
+		while(eddy)  {}						// wait to no sensor
 	}
 
-	brake();
+	brake();								// stop the motor
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+//	Function to control the motor - paramters are gains and bias
+
 void	motorControl(uint8_t kP, uint8_t kD, float kI, uint8_t b)
 {
-	dir 		= 1;
-	motorBrake 	= 0;
-	CCPR1L		= velRef;
-	ADGO 		= 1;
+	dir 		= 1;						// set motor to cw
+	motorBrake 	= 0;						// turn of motor brake
+	CCPR1L		= velRef;					// set duty cycle to reference voltage
+	ADGO 		= 1;						// start A/D conversion
 
-	while(velRef < 190)
+	while(velRef < 190)						// while reference voltage less than 190
 	{
-		for(count = 0; count < 100; count++)
+		for(count = 0; count < 100; count++) // count 100 counts of the encoder
 		{
-			while(!enc) 
+			while(!enc) 					// while not at encoder
 			{
-				setPWM(kP, kD, kI, b);
+				setPWM(kP, kD, kI, b);		// set PWM
 			}
 
-			while(enc) 
+			while(enc) 						// wait till off encoder sensor
 			{
-				setPWM(kP, kD, kI, b);
+				setPWM(kP, kD, kI, b);		// set PWM
 			}
 		}
 
-		velRef++;
+		velRef++;							// increment reference voltage
 	}
 	
-	for(count = 0; count < 6000; count++)
+	for(count = 0; count < 6000; count++)	// hold voltage for 6000 counts of the encoder
 	{
-		while(!enc) 
+		while(!enc) 						// while not at encoder
 		{
-			setPWM(kP, kD, kI, b);
+			setPWM(kP, kD, kI, b);			// set PWM
 		}
-		while(enc)  
+		while(enc)  						// wait till off encoder sensor
 		{
-			setPWM(kP, kD, kI, b);
+			setPWM(kP, kD, kI, b);			// set PWM
 		}
 	}
 
-	while(velRef > 140)
+	while(velRef > 140)						// while reference voltage greater than 140
 	{
-		for(count = 0; count < 100; count++)
+		for(count = 0; count < 100; count++) // count 100 counts of the encoder
 		{
-			while(!enc) 
+			while(!enc) 					// while not at encoder
 			{
-				setPWM(kP, kD, kI, b);
+				setPWM(kP, kD, kI, b);		// set PWM
 			}
 
-			while(enc) 
+			while(enc) 						// wait till off encoder sensor
 			{
-				setPWM(kP, kD, kI, b);
+				setPWM(kP, kD, kI, b);		// set PWM
 			}
 		}
-		velRef--;
+		velRef--;							// decrement reference voltage
 	}
 
-	brake();
+	brake();								// turn on brake
 }
+
+//	Function to set PWM Duty cyle
 
 void	setPWM(uint8_t KP, uint8_t KD, float KI, uint8_t B)
 {
-	if(readings == 64)
-	{
-		speed = tachSpeed / 64;
-		tachSpeed = 0;
+	if(readings == 64)						// check if 64 tachometer readings
+	{										// if yes
+		speed = tachSpeed / 64;				// average the readings
+		tachSpeed = 0;						// set tachometer readings to zero
 
-		errorD = error;
-		errorI += error;
-		error = velRef - speed;
+		errorD = error;						// set previous error
+		errorI += error;					// add previous error to total error
+		error = velRef - speed;				// find new error
 		
-		PORTB = error;
+		PORTB = error;						// display error on Port B
+
+		//	Equation for input with all gains and bias
+		//	Proportional - Kp * error
+		//	Derivative - Kd * (error - (previous error))
+		// 	Integral - Ki * (total error)
+		//	Bias - add on bias
 
 		input = (int) error*KP + (error - errorD)*KD + (errorI)*KI + B;	
 
-		if(input > 255)
+
+		if(input > 255)						// if duty cycle above 255
 		{
-			CCPR1L = 255;
+			CCPR1L = 255;					// set at 255
 		}
-		else if(input < 0)
+		else if(input < 0)					// if duty cycle negative
 		{
-			CCPR1L = 0;
+			CCPR1L = 0;						// set at 0
 		}		
 
 		else
 		{
-			CCPR1L = input;
+			CCPR1L = input;					// set duty cycle to command input
 		}
 
-		readings = 0; 
-		ADGO = 1;
+		readings = 0; 						// set readings to 0
+		ADGO = 1;							// start A/D conversion
 	}
 }
 
-void	brake(void)
+void	brake(void)							// Function to brake motors
 {
-	motorBrake = 1;
-	CCPR1L = 0x00;
+	motorBrake = 1;							// turn on brake
+	CCPR1L = 0x00;							// set duty cycle to 0
 }
 
-void	delay(uint8_t t)							//	delay for loop
+void	delay(uint8_t t)					//	delay loop - parameter unsigned char
 {
 	for (i = t; i > 0; i--) {}				
 }
 
-void	longTimer(void)						
-{
-	for(timer = 38461; timer > 0; timer--) {}		// 1s delay loop
+void	longTimer(void)						// 1 sec delay loop			
+{	
+	for(timer = 38461; timer > 0; timer--) {}
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+//	Interrupt Function - occurs when A/D flag set
+
 void interrupt	isr(void)
 {
-	ADIF	= 0;
-	if(readings < 64)
+	ADIF	= 0;							// reset A/D interrupt flag
+	if(readings < 64)						// check if at 64 readigns
 	{
-		readings++;
-		tachSpeed = tachSpeed + ADRES;
-		for (i = 1; i > 0; i--) {}	
-		ADGO = 1;
+		readings++;							// increment readings count
+		tachSpeed = tachSpeed + ADRES;		// add current output to tachmoter readings
+		for (i = 1; i > 0; i--) {}			// delay loop for A/D
+		ADGO = 1;							// start A/D conversion
 	}
 }
--- C/DC Motor/DCMotor.h
+++ C/DC Motor/DCMotor.h
@@ -21,60 +21,61 @@ for code to compile easier.
 #define blackButton	RA4
 
 //	Port C
-#define dir			RC0
-#define motorBrake	RC1
-#define PWM			RC2
-#define eddy		RC3
-#define LSBHex		RC4
-#define MSBHex		RC5
-#define enc			RC6
+#define dir			RC0					// Motor Direction
+#define motorBrake	RC1					// Motor Brake
+#define PWM			RC2					// PWM set
+#define eddy		RC3					// Eddy Sensor
+#define LSBHex		RC4					// lower bit of mode switch
+#define MSBHex		RC5					// upper bit of mode switch
+#define enc			RC6					// encoder sensor
 
 
 //	Type Definitions
-typedef	unsigned char	uint8_t;
-typedef	unsigned int	uint16_t;
+typedef	unsigned char	uint8_t;		// unsigned, 8 bit integer
+typedef	unsigned int	uint16_t;		// unsigned, 16 bit integer
 
 //	Variables
 unsigned long  timer;					// Holds value for 1 sec timer
-uint8_t i;			
+uint8_t i;								// unsigned char for timer	
 
-uint8_t mode;
+uint8_t mode;							// unsigned char for mode
 
-static uint8_t eddyCount;		
-static uint16_t	count;
-static uint8_t readings;			
+uint8_t eddyCount;						// unsined char for eddy sensor count
+static uint16_t	count;					// unsigned int for encoder count
+static uint8_t readings;				// unsigned char for tach readings counts
 
-//	Errors and Controller Output
-static int error;
-static int errorD;
-static int errorI;
-int input;
+//	Errors and Controller Output	
+static int error;						// unsigned char for error
+static int errorD;						// usinged char for previous error
+static int errorI;						// unsinged char for total error
+int input;								// signed integer for inputed duty cycle
+	
 
-static uint8_t desVel;
-static uint16_t speed;
-static uint16_t tachSpeed;
-uint8_t velRef;
+static uint16_t speed;					// unsinged int for tachometer speed
+static uint16_t tachSpeed;				// unsigned int for total of tachometer readings
+uint8_t velRef;							// unsigned char for desired voltage
 
 //	Gains
-static uint8_t kp;
-static uint8_t kd;
-static float  ki;
-static uint8_t bias;
+uint8_t kp;								// unsigned char for proportional gain
+uint8_t kd;								// unsigned char for derivative gain
+float  ki;								// float for integer gain
+static uint8_t bias;					// unsigned char for bias voltage
 
 //	Function Declarations
 
+//	Function declartations for Motor Contry, PWM duty cycle set, and motor brake
 void	motorControl(uint8_t, uint8_t, float, uint8_t);
 void 	setPWM(uint8_t, uint8_t, float, uint8_t);
 void	brake(void);
 
 //	Initialization
 
-void	init(void);
-void	initAtoD(void);
-void 	initPWM(void);
-void	initISR(void);
-void	refVoltage(void);
+void	init(void);						// main initialization function
+void	initAtoD(void);					// A/D initialization
+void 	initPWM(void);					// PWM initialization
+void	initISR(void);					// ISR initialization
+void	refVoltage(void);				// set reference voltage
 
 //	Delays
-void	delay(uint8_t);
-void	longTimer(void);
+void	delay(uint8_t);					// delay for max of i = 255
+void	longTimer(void);				// delay for 1 second timer
